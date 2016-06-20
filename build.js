8992ea5ec508f4da6a053794b997ca930d2abbd1
Blake Embrey <hello@blakeembrey.com>
2016-06-20 09:17:56 -0700

    Escape RegExp source unicode characters

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 var fs = require('fs')
 var join = require('path').join
 var XRegExp = require('xregexp')
+var jsesc = require('jsesc')
 
 /**
  * Write regular expressions to a file for reuse. Avoids requiring XRegExp as
@@ -10,7 +11,7 @@ var XRegExp = require('xregexp')
  * @param {RegExp} regexp
  */
 var write = function (filename, regexp) {
-  var content = 'module.exports = ' + regexp.toString() + '\n'
+  var content = 'module.exports = ' + jsesc(regexp.toString()) + '\n'
   var path = join(__dirname, 'vendor', filename)
 
   return fs.writeFileSync(path, content)
